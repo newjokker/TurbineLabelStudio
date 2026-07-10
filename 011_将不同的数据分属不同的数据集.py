3ba6e55450c4d9dc9c48f54002b9c9c15027df0c
jokker <747225581@qq.com>
2026-07-10 10:45:27 +0800

    feat:完善数据集的测试

--- 011_将不同的数据分属不同的数据集.py
+++ 011_将不同的数据分属不同的数据集.py
@@ -1,16 +1,31 @@
 
-from dao.dataset import get_dataset_info_by_name
-from dao.buc_dataset import get_bucs_not_assign_dataset, get_bucs_by_dataset_name
+from dao.dataset import get_dataset_info_by_name, get_all_datasets
+from dao.buc_dataset import get_bucs_not_assign_dataset, get_bucs_by_dataset_name, add_buc_dataset
 
 
-print(get_dataset_info_by_name("测试"))
+dataset_name = "for_ldq"
+
+dataset_info = get_dataset_info_by_name(dataset_name)
+dataset_id = dataset_info["id"]
+
+# # info = get_bucs_not_assign_dataset()
+
+# # print(len(info))
 
 
-info = get_bucs_not_assign_dataset()
-
-print(len(info))
+# res = get_all_datasets()
 
 
+# print(res)
+
+
+# info = get_bucs_not_assign_dataset()
+
+# for each_buc in info[:50]:
+    
+#     add_buc_dataset(dataset_id=dataset_id, buc=each_buc)
+
+print(get_bucs_by_dataset_name(dataset_name))
 
 
 
